013dedd1ff621bea1b29c48c72943e84da2604a5
123210233 JOAO VITOR DE SOUZA NEVES <joao.vitor.souza.neves@lcc1-42.lcc.ufcg.edu.br>
2024-02-27 16:01:59 -0300

    Improved v2
    Projeto atualizado com planejamento registrado na máquina LCC, container base como header e novo butão à adicionar interação

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 var titulo = document.getElementById("title")
-var botao = document.getElementById("trocar-title")
+var botao_trocar = document.getElementById("trocar-title")
+var botao_confirmar = document.getElementById("confirm-table")
 let trc_nome
 // const valores_inválidos = [" ","@","#","!","$"] 
 
-botao.onclick = function(){
+botao_trocar.onclick = function(){
     trc_nome = document.createElement("INPUT");
     trc_nome.setAttribute("type","text");
     trc_nome.setAttribute("value","Insira o novo nome");
     document.appendChild(trc_nome)
 }
+
+function Criar_lista(){
+    if (document.getElementById("lista").style.display === "none"){
+        document.getElementById("lista").style.display = "block"
+    } else{
+        document.getElementById("lista").style.display = "none"
+    };
+}
+
+// working on this ==> botao_confirmar.onclick = Criar_lista();
